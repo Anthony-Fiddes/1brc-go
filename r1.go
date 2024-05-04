3fe9602a21f7fadab50be363115954c46bbf74ae
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-05-04 13:56:46 -0700

    Add a little write up on r1.go about why it was so slow.

--- r1.go
+++ r1.go
@@ -1,6 +1,3 @@
-// Created without looking at a profiler at all. It ends up being twice as slow.
-// After looking at a profiler, it's obvious to see that the csv package is the
-// the problem.
 package main
 
 import (
@@ -52,6 +49,24 @@ func processRows(rows <-chan []string, results chan<- map[string]Stats) {
 
 var numWorkers = runtime.GOMAXPROCS(0)
 
+// Created without looking at a profiler at all. It ends up being about twice as
+// slow. After looking at the profiler, it's clear that this approach wouldn't
+// save a lot of time, since the work done by processRows took up way less time
+// than the Read calls initialized by the CSV reader.
+//
+// Examples online open multiple file handles at the same time and seek to
+// different parts of the file. My first instinct was that having a single large
+// read would be the fastest way to access the data. I found a lovely source on
+// how SSDs work and it appears to agree:
+// https://codecapsule.com/2014/02/12/coding-for-ssds-part-5-access-patterns-and-system-optimizations/
+//
+// But I can't argue with the results that people are getting. Maybe the
+// difference is that we're likely working on NVME SSDs where the article above
+// may be focusing on SATA SSDs?
+//
+// Indeed as I search around more, this more recent paper suggests that they
+// needed >100 concurrent read requests to saturate one NVME SSD:
+// https://www.vldb.org/pvldb/vol16/p2090-haas.pdf.
 func r1(measurementsPath string) {
 	file, err := os.Open(measurementsPath)
 	if err != nil {
